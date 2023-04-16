6be002cb6aca3c40bb96fc35a2be66b1dff1132e
Marta Młyńska <martiimly@gmail.com>
2023-04-16 12:15:22 +0200

    apka 3

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,18 @@
   <div class="my-app">
     <div v-if="loggedIn">
       <h1>Witaj w systemie do zapisów na zajęcia</h1>
-      <p>Witaj {{email}} !</p>
+      <p>Witaj {{ authenticatedUsername }} !</p>
       <button @click="logMeOut">Wyloguj</button></div>
 
     <div v-else>
-      <h1>Witaj w systemie do zapisów na zajęcia</h1>
-      <div>Zaloguj się e-mailem <input type="email" v-model="email">
-        <button @click="logMeIn">Wchodzę</button></div>
+<!--      <h1>Witaj w systemie do zapisów na zajęcia</h1>-->
+<!--      <div>Zaloguj się e-mailem <input type="email" v-model="email">-->
+<!--        <button @click="logMeIn">Wchodzę</button></div>-->
+      <LoginForm @login="(username) => logMeIn(username)" button-label="Wejdź"></LoginForm>
+      <LoginForm @login="(username) => logMeIn(username)" button-label="Wleć"></LoginForm>
+      <LoginForm @login="(username) => logMeIn(username)" :button-label="Math.random() < 0.5 ? 'Etykieta A' : 'Etykieta B'"></LoginForm>
+
+<!--      <LoginForm @login="(username) => logMeIn(username)"></LoginForm>-->
     </div>
   </div>
 <!--    <div v-if="email.length < 10">({{email.length}}) Ale masz krótki adres!</div>-->
@@ -18,19 +23,22 @@
 </template>
 
 <script>
+import LoginForm from "./LoginForm";
 import "milligram";
 export default {
+  components: {LoginForm},
   methods: {
-    logMeIn() {
+    logMeIn(username) {
+      this.authenticatedUsername = username;
       this.loggedIn = true;},
     logMeOut() {
       this.loggedIn = false;
-    this.email = ''}
+    this.authenticatedUsername = ''}
 
   },
   data() {
     return {
-      email: '',
+      authenticatedUsername: '',
       password: '',
       loggedIn: false,
     };
---
+++ src/LoginForm.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+<label>Zaloguj się e-mailem</label>
+<input type="email" v-model="authenticatedUsername">
+<button @click="enter()">Zaloguj się</button>
+</div>
+</template>
+
+<script>
+export default {
+  props: {buttonLabel: String},
+  data() {
+    return {
+      authenticatedUsername: ''
+    }
+  },
+  methods: {
+    enter() {
+      this.$emit('login', this.authenticatedUsername);
+
+    }
+  }
+}
+</script>
